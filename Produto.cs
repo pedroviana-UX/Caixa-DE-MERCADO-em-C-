28d1080774faba2b9d1af56f2890e8cd8fc86104
pedroviana-UX <pedrovianafei@gmail.com>
2025-11-20 12:50:10 -0300

    Criando classe Produto e configurando estrutura inicial

---
+++ Produto.cs
@@ -0,0 +1,13 @@
+public class Produto
+{
+    public int Id { get; set;}
+    public string? Nome { get;}
+    public decimal Preco { get;}
+
+    public Produto(int Id, string? Nome, decimal Preco)
+    {
+        Id = id; Nome = nome; Preco = preco;
+
+
+    }
+}
